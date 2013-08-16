41347a3a84561e6b093b93aed4d5d246810b96da
rajuch <ch.raju532@gmail.com>
2013-08-16 12:50:42 +0530

    added the comments

--- eigenVectorCentrality.py
+++ eigenVectorCentrality.py
@@ -60,14 +60,14 @@ def getUsers(repos, conn):
         usersSet.add(row[0])
     return usersSet
 
-# traverse the UnExploredUserQueue, for every user get the following users and followed users and add these to the graph
+# traverse the unExploredUserQueue, for every user get the following users and followed users and add these to the graph
 # and to the queue
 def createUserConnectedGraph():
     global userConnectedGraph
-    global UnExploredUserQueue
-    print 'queuesize::', UnExploredUserQueue.qsize()
-    while UnExploredUserQueue.empty()== False:
-        user=UnExploredUserQueue.get()
+    global unExploredUserQueue
+    print 'queuesize::', unExploredUserQueue.qsize()
+    while unExploredUserQueue.empty()== False:
+        user=unExploredUserQueue.get()
         if user not in ExploredUserMap:
             ExploredUserMap[user]=1
         
@@ -81,30 +81,38 @@ def createUserConnectedGraph():
             addOutGoingEdges(user, followingUsersList)
             print len(followersList), len(followingUsersList)
             for followUser in followersList:
-                UnExploredUserQueue.put(followUser)
+                unExploredUserQueue.put(followUser)
             for followingUser in followingUsersList:
-                UnExploredUserQueue.put(followingUser)
-            print 'queueSize::', UnExploredUserQueue.qsize()
+                unExploredUserQueue.put(followingUser)
+            print 'queueSize::', unExploredUserQueue.qsize()
             
 conn =mdb.connect(host="localhost",user="root",passwd="root",db="github")
 conn1 = mdb.connect(host="localhost",user="root",passwd="root",db="github_cluster")
-#repos ='1300,6276,6801,10512,10633,11394,24024,24222,24873,26019,28190,31738,38812,40323,41737,44790,51428,63074,69689,74865,79981,80013'
-repos1='1488,1920,2516,5813,6466,6654,7114,10091,10203,10282,13297,15168,15655,17886,18127,19627,19633,21987,23595,24005,24380,24809,24811,24978,25222,26243,27405,27463,28484,29244,30439,31726,36148,36325,37332,37405,37592,38513,38691,41011,41930,44032,46514,46553,47471,47982,49165,50359,50412,50445,51282,52331,53381,53404,54483,55113,55219,55633,56031,56169,56708,57625,59542,62021,62999,63017,63342,64012,64125,64230,64920,65008,65023,65790,66878,66926,67794,68574,68827,69546,71918,74633,77219,78674,81124'
 
-UnExploredUserQueue = Queue() #queue for maintaining the unexplored users
+#read the repos cluster from file
+repos =''
+try:
+    f = open('/home/raju/Work/repoCluster.txt','r')
+    repos = f.read()
+except Exception as e:
+    raise e
+
+unExploredUserQueue = Queue() #queue for maintaining the unexplored users
 ExploredUserMap = {} #map for maintaining the explored users
 
-usersSet = getUsers(repos1, conn1)
+usersSet = getUsers(repos, conn1)
 print len(usersSet)
 
+#add the users to the unExploredQueue
 for user in usersSet:
-    UnExploredUserQueue.put(user)
+    unExploredUserQueue.put(user)
 
 userConnectedGraph=nx.DiGraph()
 createUserConnectedGraph()
 
+#calculate the eigen vector centrality for userconnected graph
 centrality=nx.eigenvector_centrality(userConnectedGraph)
-#print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
+
 centralityList = []
 for node in centrality:
     class eigencentrality():
@@ -113,8 +121,10 @@ for node in centrality:
     obj.node = node
     obj.val = centrality[node]
     centralityList.append(obj)
-    
+
+#sort the the results based on the values     
 centralityList.sort(key = lambda a: a.val)
+
 f = open('/home/raju/Work/sortedcentralityOutput1','w')
 for centralityObj in centralityList:
     f.write(str(centralityObj.node)+" "+str(centralityObj.val))
